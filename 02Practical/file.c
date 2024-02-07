084eac6631286ef28f3ea96bfea484df66a590ce
CHETAN INGALE <85936690+CHETAN-INGALE@users.noreply.github.com>
2024-02-07 21:22:27 +0530

    Add files via upload

---
+++ 02Practical/file.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main() {
+	int num;
+	FILE *fptr;
+	fptr = fopen("num.txt","w");
+	if(fptr == NULL) {
+		printf("Error!");
+		exit(1);
+	}
+	printf("Enter num: ");
+	scanf("%d",&num);
+	fprintf(fptr,"%d\n",num);
+	fclose(fptr);
+	return 0;
+}
